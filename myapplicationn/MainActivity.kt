924c9f74cc2f241d6f097a8a688fbc6650b842e0
Tien160405 <tientt.23da@vku.udn.vn>
2025-10-16 23:41:01 +0700

    Add files via upload

---
+++ myapplicationn/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplicationn
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplicationn.ui.theme.MyApplicationnTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            window.statusBarColor = getColor(R.color.black)
+            HomeScreen()
+
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationnTheme {
+        Greeting("Android")
+    }
+}
